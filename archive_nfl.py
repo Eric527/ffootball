65864d205a406c12ceb761361f6d29c9b771dd66
Eric Chamberlain <echamberlain@gmail.com>
2015-10-06 17:23:19 -0400

    changed to code and data folders

--- archive_nfl.py
+++ archive_nfl.py
@@ -18,4 +18,4 @@ files = [
 CurrentDate = time.strftime("%Y%m%d")
 
 for file in files:
-    shutil.move("./output/" + file, "./output/" + file + "." + CurrentDate)
+    shutil.move("../data/output/" + file, "../data/output/archive" + file + "." + CurrentDate)
